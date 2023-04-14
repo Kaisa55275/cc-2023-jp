f5c08f2bb1fc326852856676e3ce723aa6b642b4
DAIKI NIMURA <d.nimura@titanicrising.jp>
2023-04-14 15:20:39 +0900

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,31 +19,38 @@ const STAGES: {
   [channel: string]: {
     name: string;
     color: string;
+    url?: string;
   };
 } = {
   'Channel 1 Coachella Stage': {
     name: 'Channel 1 Coachella Stage',
     color: '#6969B3',
+    url: "https://www.youtube.com/watch?v=1-3L7Uw5yEs"
   },
   'Channel 2 Outdoor Theatre': {
     name: 'Channel 2 Outdoor Theatre',
     color: '#F2C94C',
+    url: "https://www.youtube.com/watch?v=3HYVAL-52PM"
   },
   'Channel 3 Sahara': {
     name: 'Channel 3 Sahara',
     color: '#F2994A',
+    url: "https://www.youtube.com/watch?v=P8T81_n28L4"
   },
   'Channel 4 Mojave': {
     name: 'Channel 4 Mojave',
     color: '#6FCF97',
+    url: "https://www.youtube.com/watch?v=wbZ9rP74cRE"
   },
   'Channel 5 Gobi': {
     name: 'Channel 5 Gobi',
     color: '#EB5757',
+    url: "https://www.youtube.com/watch?v=M7tholO-Ob0"
   },
   'Channel 6 Sonora': {
     name: 'Channel 6 Sonora',
     color: '#2F80ED',
+    url: "https://www.youtube.com/watch?v=9hdFg3rnOXI"
   }
 }
 
@@ -96,17 +103,20 @@ const TimeTable: React.FC<TimeTableProps> = ({ timetable }) => {
       };
 
       return (
-        <div
+        <a
           key={`${channel}-${j}`}
           className="performance-container"
-          style={{ height: `${performanceHeight}px`, marginBottom: `${(marginTimeToNextPerformance)}px`, backgroundColor: STAGES[channel].color,color: calculateFontColor(STAGES[channel].color) }}
+          href={STAGES[channel].url}
+          target="_blank"
+          rel="noopener noreferrer"
+          style={{ overflow: "scroll",height: `${performanceHeight}px`, marginBottom: `${(marginTimeToNextPerformance)}px`, backgroundColor: STAGES[channel].color,color: calculateFontColor(STAGES[channel].color), textDecoration: 'none' }}
         >
           <div className="performance-content">
             <div className="artist-name">{performance.artist}</div>
             <div className="time">{performance.start_time}-{performance.end_time || ''}</div>
             <div className="channel">{channel}</div>
           </div>
-        </div>
+        </a>
       );
     });
   };
@@ -161,8 +171,6 @@ export default function Home() {
   }, [day, router]);
   
   const timetable  = timetables[day!]
-
-  console.log({ timetable})
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
